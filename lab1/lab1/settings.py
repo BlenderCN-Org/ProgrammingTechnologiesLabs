a54036f55081c21fa678e4cbb048bf4ab2fbaaba
oleg <lolego1601@gmail.com>
2017-11-05 22:40:21 +0200

    update settings for heroku deploy

--- lab1/lab1/settings.py
+++ lab1/lab1/settings.py
@@ -1,6 +1,8 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -126,3 +128,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
